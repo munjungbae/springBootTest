9b6d660ca96eb38308a2e97236f4e487e18e0b67
munjungbae <ster12390@naver.com>
2025-01-10 17:48:31 +0900

    컨트롤러 수정

--- DevProject/src/main/java/com/kh/controller/MemberController.java
+++ DevProject/src/main/java/com/kh/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.kh.controller;
 
 import java.io.File;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -128,6 +131,66 @@ public class MemberController {
 		log.info("member.getUserName() = " + member.getUserName());
 		log.info("member.getEmail() = " + member.getEmail());
 		return "errorsResult";
+	}
+
+	@RequestMapping(value = "/registerValidation", method = RequestMethod.POST)
+	public String registerValidation(@Validated Member member, BindingResult result) {
+		log.info("registerValidation");
+		if (result.hasErrors()) {
+			return "registerValidationForm"; // 뷰 파일명
+		}
+		log.info("member.getUserId() = " + member.getUserId());
+		log.info("member.getUserName() = " + member.getUserName());
+		log.info("member.getGender() = " + member.getGender());
+		return "Home";
+	}
+
+	@RequestMapping(value = "/registerValidationForm01", method = RequestMethod.GET)
+	public String registerForm01(Model model) {
+		log.info("registerValidationForm01");
+		model.addAttribute("member", new Member());
+		return "registerValidationForm"; // 뷰 파일명
+	}
+
+	@RequestMapping(value = "/registerValidation2", method = RequestMethod.POST)
+	public String registerValidation2(@Validated Member member, BindingResult result) {
+		log.info("registerValidation2");
+		// 입력값 검증 에러가 발생한 경우 true를 반환한다.
+		log.info("result.hasErrors() = " + result.hasErrors());
+		// 입력값 검증 후 BindingResult가 제공하는 메서드를 이용하여 검사 결과를 확인한다.
+		if (result.hasErrors()) {
+			List<ObjectError> allErrors = result.getAllErrors();
+			List<ObjectError> globalErrors = result.getGlobalErrors();
+			List<FieldError> fieldErrors = result.getFieldErrors();
+			log.info("allErrors.size() = " + allErrors.size());
+			log.info("globalErrors.size() = " + globalErrors.size());
+			log.info("fieldErrors.size() = " + fieldErrors.size());
+			for (int i = 0; i < allErrors.size(); i++) {
+				ObjectError objectError = allErrors.get(i);
+				log.info("allError = " + objectError);
+			}
+			for (int i = 0; i < globalErrors.size(); i++) {
+				ObjectError objectError = globalErrors.get(i);
+				log.info("globalError = " + objectError);
+			}
+			for (int i = 0; i < fieldErrors.size(); i++) {
+				FieldError fieldError = fieldErrors.get(i);
+				log.info("fieldError = " + fieldError);
+				log.info("fieldError.getDefaultMessage() = " + fieldError.getDefaultMessage());
+			}
+			return "registerValidationForm2";
+		}
+		log.info("member.getUserId() = " + member.getUserId());
+		log.info("member.getGender() = " + member.getGender());
+		log.info("member.getDateOfBirth() = " + member.getDateOfBirth());
+		return "Home";
+	}
+
+	@RequestMapping(value = "/registerValidationForm2", method = RequestMethod.GET)
+	public String registerValidation2Form01(Model model) {
+		log.info("registerValidation2Form01");
+		model.addAttribute("member", new Member());
+		return "registerValidationForm2";
 	}
 
 }
--- DevProject/src/main/java/com/kh/domain/Member.java
+++ DevProject/src/main/java/com/kh/domain/Member.java
@@ -3,7 +3,12 @@ package com.kh.domain;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 //@Getter // lombok 사용하여 getter 생성
@@ -19,10 +24,21 @@ import lombok.Data;
 public class Member {
 	@NotBlank (message = "공백이나 빈칸일 수 없습니다.")
 	private String userId;
+	@NotBlank (message = "공백이나 빈칸일 수 없습니다.")
 	private String password;
+	
+	@NotBlank
+	@Size(max=3, message="3자 미만작성 가능합니다.")
+	private String userName;
+	
+	@Email
+	private String email;
 	private List<String> hobbyList;
 	private int coin;
+	
+	@Past
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date dateOfBirth;
-	private String email;
-	private String userName;
+	
+	private String gender;
 }
