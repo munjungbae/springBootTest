a65a70b2faa6a4f788d9b948f8b278492253bce0
munjungbae <ster12390@naver.com>
2025-01-13 16:39:43 +0900

    각 게시판 생성 및 MunProject 기본설정

---
+++ MunProject/src/main/java/com/mun/MunProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunProjectApplication.class, args);
+	}
+
+}
---
+++ MunProject/src/test/java/com/mun/MunProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
