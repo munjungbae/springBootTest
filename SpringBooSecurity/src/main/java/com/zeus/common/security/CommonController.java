3c99f04a3ed28ee325957a835f9eb5021a575f49
munjungbae <ster12390@naver.com>
2025-01-16 16:31:24 +0900

    Mybatis Spring Security 적용 테스트 생성 및 인증/인가 테스트 생성

---
+++ SpringBooSecurity/src/main/java/com/zeus/common/security/CommonController.java
@@ -0,0 +1,22 @@
+package com.zeus.common.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Controller
+public class CommonController {
+	
+	@RequestMapping(value = "accessError", method=RequestMethod.GET)
+	public void accessDenied(Authentication auth, Model model) {
+		log.info("access Denied : " + auth);
+		model.addAttribute("msg" , "인증되지 않았습니다");
+	}
+	
+}
