851519c0786c88e961179236af7c5754fdc9cecc
munjungbae <ster12390@naver.com>
2025-01-20 17:51:39 +0900

    mybatis 사용 개인 쇼핑홈페이지 생성 테스트

---
+++ ImageShop/src/main/java/com/project/service/CodeGroupServiceImpl.java
@@ -0,0 +1,13 @@
+package com.project.service;
+
+import org.springframework.stereotype.Service;
+
+import com.project.mapper.CodeGroupMapper;
+
+
+@Service
+public class CodeGroupServiceImpl implements CodeGroupService {
+
+	private CodeGroupMapper mapper;
+}
+
